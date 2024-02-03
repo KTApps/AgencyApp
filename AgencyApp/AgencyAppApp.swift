e7bbd51fc18eed7928ff9bc96bb8537eb422ad67
C23097798 <mahajak@cardiff.ac.uk>
2024-02-03 17:42:15 +0000

    Initial Commit

---
+++ AgencyApp/AgencyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgencyAppApp.swift
+//  AgencyApp
+//
+//  Created by Kelvin Mahaja on 03/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AgencyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
